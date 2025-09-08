376c4e0ec62dcc38c8e51a2a9be5124146b173e6
ath31st <ath31st@yandex.ru>
2025-09-08 12:55:35 +0300

    add logic for update and load config, changing post limits, clearing access and refresh tokens

--- src/menu_handler.rs
+++ src/menu_handler.rs
@@ -1,23 +1,31 @@
+use crate::config;
 use crate::post_handler;
 use crate::{cli, parser};
 use anyhow::{Context, Result};
 use boosty_api::api_client::ApiClient;
 
-pub async fn handle_menu(client: &ApiClient, posts_limit: i32) -> Result<bool> {
+pub async fn handle_menu(client: &ApiClient) -> Result<bool> {
     cli::show_menu();
     let selected_menu = cli::read_input_menu();
 
     match selected_menu {
         1 => {
+            let cfg = config::load_config().await?;
             let input = cli::read_user_input(cli::ENTER_PATH);
-            if let Err(e) = process_boosty_url(client, posts_limit, &input).await {
+            if let Err(e) = process_boosty_url(client, cfg.posts_limit, &input).await {
                 cli::print_error(&e)
             };
         }
         2 => {
             let entered_token = cli::read_user_input(cli::ENTER_ACCESS_TOKEN);
             client.set_bearer_token(&entered_token).await?;
-            cli::show_api_client_headers(&client.headers_as_map());
+            config::update_config(|cfg| {
+                cfg.access_token = entered_token;
+                cfg.refresh_token = String::new();
+                cfg.device_id = String::new();
+            })
+            .await
+            .with_context(|| "Failed to update config")?;
         }
         3 => {
             let entered_token = cli::read_user_input(cli::ENTER_REFRESH_TOKEN);
@@ -25,8 +33,46 @@ pub async fn handle_menu(client: &ApiClient, posts_limit: i32) -> Result<bool> {
             client
                 .set_refresh_token_and_device_id(&entered_token, &entered_device_id)
                 .await?;
+            config::update_config(|cfg| {
+                cfg.access_token = String::new();
+                cfg.refresh_token = entered_token;
+                cfg.device_id = entered_device_id;
+            })
+            .await
+            .with_context(|| "Failed to update config")?;
         }
         4 => {
+            client.clear_refresh_and_device_id().await;
+            config::update_config(|cfg| {
+                cfg.access_token = String::new();
+                cfg.refresh_token = String::new();
+                cfg.device_id = String::new();
+            })
+            .await
+            .with_context(|| "Failed to clear tokens")?;
+            cli::tokens_and_client_id_cleared();
+        }
+        5 => {
+            let cfg = config::load_config().await?;
+            let prompt = format!("{} (current: {}):", cli::ENTER_POSTS_LIMIT, cfg.posts_limit);
+            let entered_posts_limit = cli::read_user_input(&prompt);
+            match entered_posts_limit
+                .trim()
+                .parse::<usize>()
+                .map_err(anyhow::Error::from)
+            {
+                Ok(limit) => {
+                    config::update_config(|cfg| cfg.posts_limit = limit)
+                        .await
+                        .with_context(|| "Failed to update posts limit")?;
+                }
+                Err(e) => {
+                    cli::print_error(&e);
+                }
+            }
+        }
+        6 => cli::show_api_client_headers(&client.headers_as_map()),
+        7 => {
             cli::exit_message();
             return Ok(false);
         }
@@ -35,7 +81,7 @@ pub async fn handle_menu(client: &ApiClient, posts_limit: i32) -> Result<bool> {
     Ok(true)
 }
 
-async fn process_boosty_url(client: &ApiClient, posts_limit: i32, input: &str) -> Result<()> {
+async fn process_boosty_url(client: &ApiClient, posts_limit: usize, input: &str) -> Result<()> {
     let parsed = parser::parse_boosty_url(input)
         .with_context(|| format!("Failed to parse Boosty URL '{input}'"))?;
 
