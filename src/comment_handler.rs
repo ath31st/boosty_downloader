eff1f5938d05e1c29cd81ecec6aeaee0a33a9cfc
ath31st <ath31st@yandex.ru>
2025-10-16 10:28:30 +0300

    working on comment processing v1

--- src/comment_handler.rs
+++ src/comment_handler.rs
@@ -1,36 +1,39 @@
+use std::path::PathBuf;
+
 use anyhow::{Context, Result};
 use boosty_api::api_response::CommentsResponse;
 
-use crate::cli;
+use crate::{cli, file_handler};
 
 pub struct CommentsResult {
     pub response: CommentsResponse,
-    pub safe_post_title: String,
     pub blog_url: String,
+    pub safe_post_title: String,
+    pub created_at: i64,
 }
 
 pub async fn process_comments(results: Vec<CommentsResult>) -> Result<()> {
     for result in results {
-        process(&result.response, &result.safe_post_title, &result.blog_url)
-            .await
-            .with_context(|| {
-                format!(
-                    "Error processing comments for post '{}'",
-                    result.safe_post_title
-                )
-            })?;
+        process(&result).await.with_context(|| {
+            format!(
+                "Error processing comments for post '{}'",
+                result.safe_post_title
+            )
+        })?;
     }
     Ok(())
 }
 
-async fn process(
-    comment_response: &CommentsResponse,
-    safe_post_title: &str,
-    blog_url: &str,
-) -> Result<()> {
-    if !check_available_comments(comment_response, safe_post_title) {
+async fn process(cr: &CommentsResult) -> Result<()> {
+    if !check_available_comments(&cr.response, &cr.safe_post_title) {
         return Ok(());
     }
+
+    let post_folder_path: PathBuf =
+        file_handler::prepare_folder_path(&cr.blog_url, &cr.safe_post_title, cr.created_at).await?;
+
+    let comments_folder_path: PathBuf =
+        file_handler::prepare_folder_path_for_comments(&post_folder_path).await?;
     Ok(())
 }
 
