a484dbcdba828fe233410ff2450e87ce99af52f6
ath31st <ath31st@yandex.ru>
2025-10-17 13:18:46 +0300

    add top level processing comments

--- src/comment_handler.rs
+++ src/comment_handler.rs
@@ -1,9 +1,14 @@
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
-use boosty_api::api_response::CommentsResponse;
+use boosty_api::{
+    api_response::CommentsResponse,
+    media_content::ContentItem,
+    traits::{HasContent, IsAvailable},
+};
+use chrono::{DateTime, Utc};
 
-use crate::{cli, file_handler};
+use crate::{cli, content_items_handler, file_handler};
 
 pub struct CommentsResult {
     pub response: CommentsResponse,
@@ -34,11 +39,54 @@ async fn process(cr: &CommentsResult) -> Result<()> {
 
     let comments_folder_path: PathBuf =
         file_handler::prepare_folder_path_for_comments(&post_folder_path).await?;
+
+    let items: Vec<ContentItem> = cr
+        .response
+        .data
+        .iter()
+        .filter(|c| !c.not_available())
+        .flat_map(|c| {
+            let mut items = c.extract_content();
+            modify_text_content(&mut items, &c.author.name, c.created_at);
+            items
+        })
+        .collect();
+
+    content_items_handler::process_content_items(
+        items,
+        &cr.safe_post_title,
+        &comments_folder_path,
+        None,
+    )
+    .await?;
+
     Ok(())
 }
 
+fn modify_text_content(items: &mut Vec<ContentItem>, author: &str, created_at: u64) {
+    for item in items {
+        match item {
+            ContentItem::Text {
+                content,
+                modificator,
+            } if modificator != "BLOCK_END"
+                && !content.is_empty()
+                && content.starts_with("[\"") =>
+            {
+                let datetime: DateTime<Utc> =
+                    DateTime::from_timestamp(created_at as i64, 0).unwrap_or_default();
+
+                let date_str = datetime.format("%Y.%m.%d %H:%M").to_string();
+                let insert_pos = 2;
+                content.insert_str(insert_pos, &format!("[{date_str}][{author}] "));
+            }
+            _ => {}
+        }
+    }
+}
+
 fn check_available_comments(comments: &CommentsResponse, post_title: &str) -> bool {
-    if comments.data.is_empty() {
+    if comments.data.is_empty() || comments.data.iter().all(|c| c.not_available()) {
         cli::comments_for_post_empty_or_not_available(post_title);
         return false;
     }
