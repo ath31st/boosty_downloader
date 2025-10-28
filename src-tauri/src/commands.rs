c69259d7921461bccea0077701c33d281b3a5928
ath31st <ath31st@yandex.ru>
2025-10-28 13:35:20 +0300

    massive update, trying config rust-side

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,37 @@
+use std::sync::Arc;
+
+use boosty_downloader_core::AppConfig;
+use tauri::State;
+use tokio::sync::Mutex;
+
+use crate::state::AppState;
+
+#[tauri::command]
+pub async fn get_config(state: State<'_, Arc<Mutex<AppState>>>) -> Result<AppConfig, String> {
+    let state = state.lock().await;
+    Ok(state.config.clone())
+}
+
+#[tauri::command]
+pub async fn update_config(
+    state: State<'_, Arc<Mutex<AppState>>>,
+    new_config: AppConfig,
+) -> Result<(), String> {
+    let mut state = state.lock().await;
+    state.config = new_config;
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn init_client(state: State<'_, Arc<Mutex<AppState>>>) -> Result<(), String> {
+    let client = boosty_downloader_core::make_client()
+        .await
+        .map_err(|e| e.to_string())?;
+    boosty_downloader_core::init_client(&client)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    let mut state = state.lock().await;
+    state.client = Some(client);
+    Ok(())
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,40 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::sync::Arc;
+
+//use tauri::Manager;
+use tokio::sync::Mutex;
+
+use crate::state::AppState;
+
+mod commands;
+mod state;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let state = Arc::new(Mutex::new(AppState::default()));
+
     tauri::Builder::default()
+        .manage(state)
+        // .setup(|app| {
+        //     let app_handle = app.handle();
+        //     let state = app_handle.state::<Arc<Mutex<AppState>>>().inner().clone();
+        //     tauri::async_runtime::spawn(async move {
+        //         let client = boosty_downloader_core::make_client()
+        //             .await
+        //             .expect("Failed to create client");
+        //         boosty_downloader_core::init_client(&client)
+        //             .await
+        //             .expect("Failed to init client");
+        //         let mut state = state.lock().await;
+        //         state.client = Some(client);
+        //     });
+        //     Ok(())
+        // })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            commands::get_config,
+            commands::update_config,
+            commands::init_client
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
