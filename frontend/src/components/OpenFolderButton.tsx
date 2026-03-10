17de4d6c7ba54be87ea4450e7a6178da3f12c547
ath31st <ath31st@yandex.ru>
2026-03-10 15:46:47 +0300

    change way to invoke opening folder for saving content

--- frontend/src/components/OpenFolderButton.tsx
+++ frontend/src/components/OpenFolderButton.tsx
@@ -1,14 +1,12 @@
-import { revealItemInDir } from '@tauri-apps/plugin-opener';
+import { openPath } from '@tauri-apps/plugin-opener';
 import { Button } from '@/components/Button';
 import { invoke } from '@tauri-apps/api/core';
-import { resolve } from '@tauri-apps/api/path';
 import { FolderOpen } from 'lucide-react';
 
 export function OpenFolderButton() {
   const handleOpenFolder = async () => {
-    const exePathString = (await invoke('get_download_path', {})) as string;
-    const exePath = await resolve(exePathString);
-    await revealItemInDir(exePath);
+    const downloadPath = (await invoke('get_download_path', {})) as string;
+    await openPath(downloadPath);
   };
 
   return (
