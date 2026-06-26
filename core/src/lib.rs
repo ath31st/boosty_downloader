526fb0b8c8bdf60a79cd16307782bf574299fc15
ath31st <ath31st@yandex.ru>
2026-06-26 17:05:31 +0300

    add download options crate (for filtering allowed content for downloading) v2

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,6 +4,7 @@ pub(crate) mod comment_handler;
 pub(crate) mod config;
 pub(crate) mod console_logger;
 pub(crate) mod content_items_handler;
+pub(crate) mod download_options;
 pub(crate) mod file_handler;
 pub(crate) mod headers;
 pub(crate) mod logger;
@@ -14,8 +15,11 @@ pub(crate) mod progress_reporter;
 pub(crate) mod url_context;
 
 pub use cli::print_error;
-pub use config::{AppConfig, CommentsConfig, load_config, save_config, sync_auth, get_download_path};
+pub use config::{
+    AppConfig, CommentsConfig, get_download_path, load_config, save_config, sync_auth,
+};
 pub use console_logger::ConsoleLogger;
+pub use download_options::{DownloadOptions, default_download_options, filter_content_items};
 pub use logger::{LogLevel, LogMessage, Logger, ProgressMessage, get_logger, set_logger};
 pub use menu_handler::{handle_menu, process_boosty_url};
 pub use url_context::build_url_context;
